770d3603d1a8cf392a232704d9be0c1ff2eb4163
Jill Ference <ferencejill@gmail.com>
2024-12-16 12:03:16 -0700

    updated Grad-CAM implementation

--- app/gradcam.py
+++ app/gradcam.py
@@ -1,4 +1,4 @@
-# gradCAM implementation for front-end and back-end communication
+# Grad-CAM implementation for front-end and back-end communication
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -6,70 +6,130 @@ from torchvision import models, transforms
 import matplotlib.pyplot as plt
 from PIL import Image
 
-def load_image_not_path(img):
-    # Transformation for passing image into the network
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                            std=[0.229, 0.224, 0.225]),
-    ])
+"""
+Used Kazuto Nakashima's Grad-CAM with PyTorch implementation 
+https://github.com/kazuto1011/grad-cam-pytorch
+"""
+class _BaseWrapper(object):
+    def __init__(self, model):
+        super(_BaseWrapper, self).__init__()
+        self.device = next(model.parameters()).device
+        self.model = model
+        self.handlers = []  # A set of hook function handlers
 
-    image = img.convert('RGB')
-    image = transform(image).unsqueeze(0)
-    
-    return image
+    def _encode_one_hot(self, ids):
+        one_hot = torch.zeros_like(self.logits).to(self.device)
+        one_hot.scatter_(1, ids, 1.0)
+        return one_hot
 
+    def forward(self, image):
+        self.image_shape = image.shape[2:]
+        self.logits = self.model(image)
+        self.probs = F.softmax(self.logits, dim=1)
+        return self.probs.sort(dim=1, descending=True)  # Ordered results
 
-# GradCAM
-# Decodes importance of each feature map for a specific class by analyzing gradients in the last convolutional layer
-class GradCAM:
-    def __init__(self, model, last_layer):
-        self.model = model # Pre-trained ResNet152 Model
-        self.last_layer = last_layer # Last convolutional layer
-        self.gradients = None # Stores the gradients of the class score w/ respect to the activtions of the target layer
-        self.activations = None # Stores the feature maps (activations) from the forward pass through the target layer
-        self.hook_layers()
+    def backward(self, ids):
+        """
+        Class-specific backpropagation
+        """
+        one_hot = self._encode_one_hot(ids)
+        self.model.zero_grad()
+        self.logits.backward(gradient=one_hot, retain_graph=True)
 
-    # Gets the activations and the gradients
-    def hook_layers(self):
-        def forward_hook(module, input, output):
-            self.activations = output
+    def generate(self):
+        raise NotImplementedError
 
-        def backward_hook(module, grad_in, grad_out):
-            self.gradients = grad_out[0]
+    def remove_hook(self):
+        """
+        Remove all the forward/backward hook functions
+        """
+        for handle in self.handlers:
+            handle.remove()
 
-        self.last_layer.register_forward_hook(forward_hook)
-        self.last_layer.register_backward_hook(backward_hook)
+"""
+Used Kazuto Nakashima's Grad-CAM with PyTorch implementation 
+https://github.com/kazuto1011/grad-cam-pytorch
+"""
+class GradCAM(_BaseWrapper):
+    """
+    "Grad-CAM: Visual Explanations from Deep Networks via Gradient-based Localization"
+    https://arxiv.org/pdf/1610.02391.pdf
+    Look at Figure 2 on page 4
+    """
+
+    def __init__(self, model, candidate_layers=None):
+        super(GradCAM, self).__init__(model)
+        self.fmap_pool = {} # Stores the feature maps (activations) from the forward pass through the target layer
+        self.grad_pool = {} # Stores the gradients of the class score w/ respect to the activtions of the target layer
+        self.candidate_layers = candidate_layers # Only used last layer for our implementation
+
+        def save_fmaps(key):
+            def forward_hook(module, input, output):
+                self.fmap_pool[key] = output.detach()
+
+            return forward_hook
+
+        def save_grads(key):
+            def backward_hook(module, grad_in, grad_out):
+                self.grad_pool[key] = grad_out[0].detach()
+
+            return backward_hook
+
+        for name, module in self.model.named_modules():
+          self.handlers.append(module.register_forward_hook(save_fmaps(name)))
+          self.handlers.append(module.register_backward_hook(save_grads(name)))
+
+    def _find(self, pool, target_layer):
+        if target_layer in pool.keys():
+            return pool[target_layer]
+        else:
+            raise ValueError("Invalid layer name: {}".format(target_layer))
 
     # Generates the class activation map
-    def generate_heatmap(self, class_idx):
-        # Calculate gradients of the class score w/ respect to the activations of the target layer
-        weights = torch.mean(self.gradients, dim=(2, 3))  # Global average pooling of gradients
-        grad_cam = torch.sum(weights[:, :, None, None] * self.activations, dim=1) # Weighted sum of feature maps
-        grad_cam = F.relu(grad_cam)  # Apply ReLU
-        grad_cam = grad_cam.squeeze().cpu().detach().numpy()
+    def generate_heatmap(self, target_layer):
+        fmaps = self._find(self.fmap_pool, target_layer)
+        grads = self._find(self.grad_pool, target_layer)
+        weights = F.adaptive_avg_pool2d(grads, 1)
 
-        # Normalize the heatmap to a range [0, 1]
-        grad_cam -= grad_cam.min()
-        grad_cam /= grad_cam.max()
-        return grad_cam
+        gcam = torch.mul(fmaps, weights).sum(dim=1, keepdim=True)
 
-    def __call__(self, image, class_idx):
-        # Forward pass
+        # Added to solve missing heatmaps 
+        # Balances between positive and negative contributions towards target class
+        max_value = gcam.max()
+        if max_value > 0:
+            gcam = F.relu(gcam) # Apply ReLU if there are positive contributions
+        # Don't apply ReLU if there are only negative contributions
+        print(f"Maximum Grad-CAM score for the predicted class: {max_value}")
+
+        gcam = F.interpolate(
+            gcam, self.image_shape, mode="bilinear", align_corners=False
+        )
+
+        B, C, H, W = gcam.shape
+        gcam = gcam.view(B, -1)
+        gcam -= gcam.min(dim=1, keepdim=True)[0]
+        gcam /= gcam.max(dim=1, keepdim=True)[0]
+        gcam = gcam.view(B, C, H, W)
+
+        return gcam
+
+
+    def __call__(self, image, class_idx, target_layer):
+        """Performs forward and backward pass to compute Grad-CAM."""
+        self.image_shape = image.shape[2:]  # Get the spatial size of the input image
         output = self.model(image)
 
         # Zero gradients
         self.model.zero_grad()
 
-        # Backward pass
+        # Perform backward pass for the specified class
         output[:, class_idx].backward()
 
-        # Generate heatmap
-        heatmap = self.generate_heatmap(class_idx)
+        # Generate and return the heatmap
+        heatmap = self.generate_heatmap(target_layer)
         return heatmap
 
-def overlay_heatmap_not_path(heatmap, img, alpha=0.6):
+def overlay_heatmap(heatmap, img, alpha=0.5):
     # Resize the heatmap to match the original image size
     heatmap = np.uint8(255 * heatmap)
     heatmap = Image.fromarray(heatmap).resize((224, 224), Image.LANCZOS)
